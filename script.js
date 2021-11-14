d61b9187f355ca73d0339fd4e8dba66deaa52c25
inesssik <denya.savchenko@gmail.com>
2021-11-14 13:52:48 +0200

    asd

---
+++ script.js
@@ -0,0 +1,4 @@
+let xhr = new XMLHttpRequest()
+let url = 'https://api.telegram.org/bot2128368955:AAH5CJnEtswIxku8-ZE-l3tTZ2nvEJI4fK8/sendMessage?chat_id=633180859&text=TEXT'
+xhr.open('GET', url)
+xhr.send()
